bebe0ee5d7107ef5169c74ce7d2e5d0b3639af4b
prendagast <chriscamano007@gmail.com>
2022-01-30 13:19:58 -0800

    First Push

---
+++ in_class_work.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 23:01:36 2022
+
+@author: hboateng
+"""
+class testcomprehension:
+    ## 26: (Task 0.5.28) Procedure nextInts
+    # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
+    def nextInts(L): return [ ... ]   
+    
+
+    ## 27: (Task 0.5.29) Procedure cubes
+    # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
+    def cubes(L): return [ ... ] 
+     
